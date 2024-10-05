d16c2abf4db4a3128920afe2732c58ffae28a053
Sharathchandra <sharathchandra9204@gmail.com>
2024-10-05 15:23:04 +0530

    Task 2 submitted

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const text = document.getElementById('text');
+const button1=document.getElementById('button1');
+const button2=document.getElementById('button2');
+button1.addEventListener('click',function(){
+    text.innerText='button 1 is clicked';
+});
+button2.addEventListener('click',function(){
+    text.innerText='button 2 is clicked';
+});
